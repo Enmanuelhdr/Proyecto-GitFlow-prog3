753c7b1aa1321604e63193692eb71bf7179921a7
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-07-29 21:32:02 -0400

    Feature: añadiendo funcionalidad de guardado y almacenamiento de archivos mp4

--- main.py
+++ main.py
@@ -20,3 +20,7 @@ if __name__ == '__main__':
         if extension in [".mp3"]:
             musicFolder = "C:/Users/ehdel/Music/"
             os.rename(downloadsFolder + filename, musicFolder + filename)
+
+        if extension in [".mp4"]:
+            musicFolder = "C:/Users/ehdel/Music/"
+            os.rename(downloadsFolder + filename, musicFolder + filename)
